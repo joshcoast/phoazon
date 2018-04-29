fc276c7320315f3a4ab36c5601aa74fbe5d1418a
Josh Coast <josh@joshcoast.com>
2018-04-28 19:03:06 -0600

    challenge #1

--- app.js
+++ app.js
@@ -1,19 +1,80 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-
-  // Your username
-  user: "root",
-
-  // Your password
-  password: "",
-  database: "bamazon"
+	host: "localhost",
+	port: 3306,
+	user: "root",
+	password: "",
+	database: "bamazon"
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-  connection.end();
+connection.connect(function (err) {
+	if (err) throw err;
+	console.log("connected as id " + connection.threadId);
+	listProducts();
 });
+
+function listProducts() {
+	console.log("\nAvailable Products");
+	console.log("------------------");
+	connection.query("SELECT * FROM products", function (err, res) {
+		if (err) throw err;
+		res.forEach(function (element) {
+			console.log(`ID: ${element.item_id}, Name: ${element.product_name}, Price: $${element.price}`);
+		});
+		buyProduct();
+	});
+}
+
+function buyProduct() {
+	inquirer
+		.prompt([{
+				name: "productId",
+				type: "input",
+				message: "What is the ID for the product you'd like to buy? ",
+				validate: function (value) {
+					if (isNaN(value) === false) {
+						return true;
+					}
+					return false;
+				}
+			},
+			{
+				name: "quantity",
+				type: "input",
+				message: "How many would you like to buy? ",
+				validate: function (value) {
+					if (isNaN(value) === false) {
+						return true;
+					}
+					return false;
+				}
+			}
+		])
+		.then(function (answer) {
+			var answerProductId = answer.productId;
+			var answerQuantity = answer.quantity;
+
+			connection.query(`SELECT * FROM products WHERE item_id = "${answerProductId}"`, function (err, results) {
+				if (err) throw err;
+				// results will contain the results of the query
+				var productName = results[0].product_name;
+				var productPrice = results[0].price;
+				if (results[0].stock_quantity >= answerQuantity) {
+					var newQuantity = results[0].stock_quantity - answerQuantity;
+					updateProduct(answerQuantity, newQuantity, answerProductId, productName, productPrice);
+				} else {
+					console.log(`Sorry, you wanted ${answerQuantity} and we only have ${results[0].stock_quantity}. Order canceled due to insufficient quantity!`);
+				}
+				connection.end();
+			});
+		});
+}
+
+function updateProduct(answerQuantity, newQuantity, answerProductId, productName, productPrice) {
+	connection.query(`UPDATE products SET stock_quantity = '${newQuantity}' WHERE item_id = '${answerProductId}'`, function (error) {
+		if (error) throw error;
+		console.log(`You just bought ${answerQuantity} ${productName}s for $${productPrice * answerQuantity}`);
+	});
+}
--- bamazon.sql
+++ bamazon.sql
@@ -1,7 +1,7 @@
-DROP DATABASE IF EXISTS phoazonDB;
-CREATE database phoazonDB;
+DROP DATABASE IF EXISTS bamazon;
+CREATE database bamazon;
 
-USE phoazonDB;
+USE bamazon;
 
 CREATE TABLE products (
   item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
@@ -14,12 +14,12 @@ CREATE TABLE products (
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Shirt", "Cloths", 35.50, 20),
- ("Shoes", "Cloths", 35.50, 10), 
- ("TV", "Electronics", 1200.00, 4), 
- ("iPad", "Electronics", 700.00, 7),
- ("Chair", "Home", 89.99, 14),
- ("Table", "Home", 145.00, 9), 
- ("Desk", "Home", 49.99, 5), 
- ("Power Ranger", "Toys", 29.99, 32), 
- ("My Little Pony", "Toys", 24.99, 9), 
- ("GIJoe", "Toys", 19.99, 37);
+	("Watch", "Cloths", 35.50, 10), 
+	("TV", "Electronics", 1200.00, 4), 
+	("iPad", "Electronics", 700.00, 7),
+	("Chair", "Home", 89.99, 14),
+	("Table", "Home", 145.00, 9), 
+	("Desk", "Home", 49.99, 5), 
+	("Power Ranger", "Toys", 29.99, 32), 
+	("Super Ball", "Toys", 2.99, 105), 
+	("GI Joe", "Toys", 19.99, 37);
